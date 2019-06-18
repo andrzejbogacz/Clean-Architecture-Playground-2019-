6f2c9d98343d265db5ef28fef08e0e3befa68714
Ashaneen <andrzej.bogacz@mail.com>
2019-06-18 10:23:24 +0200

    Initial commit

---
+++ presentation/src/main/java/com/example/loquicleanarchitecture/di/AppComponent.kt
@@ -0,0 +1,24 @@
+package com.example.loquicleanarchitecture.di
+
+import android.app.Application
+import com.example.loquicleanarchitecture.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component
+    (modules = [AndroidSupportInjectionModule::class])
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+}
