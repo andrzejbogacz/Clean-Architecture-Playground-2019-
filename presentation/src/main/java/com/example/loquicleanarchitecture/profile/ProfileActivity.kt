62b3f08d47b336f25b055eac7d71775241bd1516
Ashaneen <andrzej.bogacz@mail.com>
2019-07-08 16:24:20 +0200

    - profileActivity.kt with function for visual transition between photographies - finished xml part for profileActivity

---
+++ presentation/src/main/java/com/example/loquicleanarchitecture/profile/ProfileActivity.kt
@@ -0,0 +1,24 @@
+package com.example.loquicleanarchitecture.profile
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.TransitionManager
+import com.example.loquicleanarchitecture.R
+import kotlinx.android.synthetic.main.activity_profile.*
+import org.jetbrains.anko.toast
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        swapView(image1)
+    }
+
+
+    fun swapView(v: View) {
+        TransitionManager.beginDelayedTransition(layout)
+        placeholder.setContentId(v.id)
+        toast(v.id)
+    }
+}
