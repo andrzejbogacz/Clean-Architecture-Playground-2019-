b5da390b86a572047ad4d53a151d0a46698ae523
Ashaneen <andrzej.bogacz@mail.com>
2019-07-09 20:06:16 +0200

    conversion kt

---
+++ presentation/src/main/java/com/example/loquicleanarchitecture/fixtures/DialogsFixtures.kt
@@ -0,0 +1,77 @@
+package com.example.loquicleanarchitecture.fixtures
+
+import com.example.loquicleanarchitecture.model.Dialog
+import com.example.loquicleanarchitecture.model.Message
+import com.example.loquicleanarchitecture.model.User
+
+import java.util.ArrayList
+import java.util.Calendar
+import java.util.Date
+
+/*
+ * Created by Anton Bevza on 07.09.16.
+ */
+class DialogsFixtures private constructor() : FixturesData() {
+    init {
+        throw AssertionError()
+    }
+
+    companion object {
+
+        val dialogs: ArrayList<Dialog>
+            get() {
+                val chats = ArrayList<Dialog>()
+
+                for (i in 0..19) {
+                    val calendar = Calendar.getInstance()
+                    calendar.add(Calendar.DAY_OF_MONTH, -(i * i))
+                    calendar.add(Calendar.MINUTE, -(i * i))
+
+                    chats.add(getDialog(i, calendar.time))
+                }
+
+                return chats
+            }
+
+        private fun getDialog(i: Int, lastMessageCreatedAt: Date): Dialog {
+            val users = users
+            return Dialog(
+                randomId,
+                if (users.size > 1) FixturesData.groupChatTitles[users.size - 2] else users[0].name,
+                if (users.size > 1) FixturesData.groupChatImages[users.size - 2] else randomAvatar,
+                users,
+                getMessage(lastMessageCreatedAt),
+                if (i < 3) 3 - i else 0
+            )
+        }
+
+        private val users: ArrayList<User>
+            get() {
+                val users = ArrayList<User>()
+                val usersCount = 1 + FixturesData.rnd.nextInt(4)
+
+                for (i in 0 until usersCount) {
+                    users.add(user)
+                }
+
+                return users
+            }
+
+        private val user: User
+            get() = User(
+                randomId,
+                randomName,
+                randomAvatar,
+                randomBoolean
+            )
+
+        private fun getMessage(date: Date): Message {
+            return Message(
+                randomId,
+                user,
+                randomMessage,
+                date
+            )
+        }
+    }
+}
---
+++ presentation/src/main/java/com/example/loquicleanarchitecture/fixtures/MessagesFixtures.kt
@@ -0,0 +1,76 @@
+package com.example.loquicleanarchitecture.fixtures
+
+import com.example.loquicleanarchitecture.model.Message
+import com.example.loquicleanarchitecture.model.User
+
+import java.util.ArrayList
+import java.util.Calendar
+import java.util.Date
+
+/*
+ * Created by troy379 on 12.12.16.
+ */
+class MessagesFixtures private constructor() : FixturesData() {
+    init {
+        throw AssertionError()
+    }
+
+    companion object {
+
+        val imageMessage: Message
+            get() {
+                val message = Message(randomId, user, null)
+                message.setImage(Message.Image(randomImage))
+                return message
+            }
+
+        val voiceMessage: Message
+            get() {
+                val message = Message(randomId, user, null)
+                message.voice = Message.Voice("http://example.com", FixturesData.rnd.nextInt(200) + 30)
+                return message
+            }
+
+        val textMessage: Message
+            get() = getTextMessage(randomMessage)
+
+        fun getTextMessage(text: String): Message {
+            return Message(randomId, user, text)
+        }
+
+        fun getMessages(startDate: Date?): ArrayList<Message> {
+            val messages = ArrayList<Message>()
+            for (i in 0..9/*days count*/) {
+                val countPerDay = FixturesData.rnd.nextInt(5) + 1
+
+                for (j in 0 until countPerDay) {
+                    val message: Message
+                    if (i % 2 == 0 && j % 3 == 0) {
+                        message = imageMessage
+                    } else {
+                        message = textMessage
+                    }
+
+                    val calendar = Calendar.getInstance()
+                    if (startDate != null) calendar.time = startDate
+                    calendar.add(Calendar.DAY_OF_MONTH, -(i * i + 1))
+
+                    message.setCreatedAt(calendar.time)
+                    messages.add(message)
+                }
+            }
+            return messages
+        }
+
+        private val user: User
+            get() {
+                val even = FixturesData.rnd.nextBoolean()
+                return User(
+                    if (even) "0" else "1",
+                    if (even) FixturesData.names[0] else FixturesData.names[1],
+                    if (even) FixturesData.avatars[0] else FixturesData.avatars[1],
+                    true
+                )
+            }
+    }
+}
