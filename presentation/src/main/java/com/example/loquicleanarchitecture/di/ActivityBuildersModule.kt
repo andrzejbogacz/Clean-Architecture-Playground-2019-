b8c9d1d0a55af61a065bc9a5ac756a8947c50e34
Ashaneen <andrzej.bogacz@mail.com>
2019-07-10 18:34:26 +0200

    added profile Scope to @ContributesAndroidInjector module

--- presentation/src/main/java/com/example/loquicleanarchitecture/di/ActivityBuildersModule.kt
+++ presentation/src/main/java/com/example/loquicleanarchitecture/di/ActivityBuildersModule.kt
@@ -1,5 +1,6 @@
 package com.example.loquicleanarchitecture.di
 
+import com.example.loquicleanarchitecture.chat.ChatActivity
 import com.example.loquicleanarchitecture.di.auth.AuthModule
 import com.example.loquicleanarchitecture.di.auth.AuthScope
 import com.example.loquicleanarchitecture.di.auth.AuthViewModelsModule
@@ -9,9 +10,12 @@ import com.example.loquicleanarchitecture.di.chat.ChatViewModelsModule
 import com.example.loquicleanarchitecture.di.main.MainModule
 import com.example.loquicleanarchitecture.di.main.MainScope
 import com.example.loquicleanarchitecture.di.main.MainViewModelsModule
+import com.example.loquicleanarchitecture.di.profile.ProfileModule
+import com.example.loquicleanarchitecture.di.profile.ProfileScope
+import com.example.loquicleanarchitecture.di.profile.ProfileViewModelsModule
 import com.example.loquicleanarchitecture.login.AuthActivity
-import com.example.loquicleanarchitecture.login.ChatActivity
 import com.example.loquicleanarchitecture.main.MainActivity
+import com.example.loquicleanarchitecture.profile.ProfileActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -26,10 +30,13 @@ abstract class ActivityBuildersModule {
     @ContributesAndroidInjector(modules = [MainModule::class, MainViewModelsModule::class])
     internal abstract fun contributeMainActivity(): MainActivity
 
-
     @ChatScope
     @ContributesAndroidInjector(modules = [ChatModule::class, ChatViewModelsModule::class])
     internal abstract fun contributeChatActivity(): ChatActivity
+
+    @ProfileScope
+    @ContributesAndroidInjector(modules = [ProfileModule::class, ProfileViewModelsModule::class])
+    internal abstract fun contributeProfileActivity(): ProfileActivity
 
 
 }
