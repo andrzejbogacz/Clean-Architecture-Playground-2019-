5a5af034079d3527218eac7e2066c999abffd780
Ashaneen <andrzej.bogacz@mail.com>
2019-07-10 20:39:37 +0200

    extracted most of the code to abstract super class ChatlistActivity

--- presentation/src/main/java/com/example/loquicleanarchitecture/main/MainActivity.kt
+++ presentation/src/main/java/com/example/loquicleanarchitecture/main/MainActivity.kt
@@ -2,72 +2,35 @@ package com.example.loquicleanarchitecture.main
 
 import android.os.Bundle
 import android.view.Menu
-import com.example.loquicleanarchitecture.BaseActivity
 import com.example.loquicleanarchitecture.R
+import com.example.loquicleanarchitecture.chat.ChatActivity
 import com.example.loquicleanarchitecture.fixtures.DialogsFixtures
+import com.example.loquicleanarchitecture.main.chatlist.ChatlistActivity
 import com.example.loquicleanarchitecture.model.Dialog
 import com.example.loquicleanarchitecture.utils.AppUtils
-import com.google.firebase.auth.FirebaseAuth
-import com.squareup.picasso.Picasso
 import com.stfalcon.chatkit.commons.ImageLoader
 import com.stfalcon.chatkit.dialogs.DialogsListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
+import org.jetbrains.anko.startActivity
 
-class MainActivity : BaseActivity(), DialogsListAdapter.OnDialogClickListener<Dialog>,
-    DialogsListAdapter.OnDialogLongClickListener<Dialog> {
+class MainActivity : ChatlistActivity() {
 
 
-    lateinit var dialogsAdapter: DialogsListAdapter<Dialog>
-
-    @Inject
-    lateinit var firebaseAuth: FirebaseAuth
-
-    @Inject
-    lateinit var picasso: Picasso
-
-    lateinit var imageLoader: ImageLoader
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        imageLoader =
-            ImageLoader { imageView, url, payload -> picasso.load(url).into(imageView) }
         initAdapter()
-    }
-
-    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.drawer, menu)
-        return super.onPrepareOptionsMenu(menu)
 
     }
-
 
     private fun initAdapter() {
-        dialogsAdapter = DialogsListAdapter(imageLoader)
-        dialogsAdapter.setItems(DialogsFixtures.dialogs)
+     super.dialogsAdapter = DialogsListAdapter(super.imageLoader)
+     super.dialogsAdapter.setItems(DialogsFixtures.dialogs)
 
-        dialogsAdapter.setOnDialogClickListener(this)
-        dialogsAdapter.setOnDialogLongClickListener(this)
+     super.dialogsAdapter.setOnDialogClickListener(this)
+     super.dialogsAdapter.setOnDialogLongClickListener(this)
 
-        dialogsList.setAdapter(dialogsAdapter)
+        dialogsList.setAdapter(super.dialogsAdapter)
     }
-
-    override fun onDialogLongClick(dialog: Dialog) {
-        AppUtils.showToast(
-            this,
-            dialog.dialogName,
-            false
-        )
-    }
-
-    override fun onDialogClick(dialog: Dialog?) {
-        AppUtils.showToast(
-            this,
-            dialog!!.dialogName,
-            false
-        )
-    }
-
 }
