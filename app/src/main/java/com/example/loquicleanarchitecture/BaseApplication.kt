fd3a1ab96506999bf9171bda95c1f05f5a33dae5
Ashaneen <andrzej.bogacz@mail.com>
2019-07-15 20:01:12 +0200

    updated view_profile_user_details with scroll view updated views to be compatible with multiple densities updated drawer header added few strings transparent status bar

--- app/src/main/java/com/example/loquicleanarchitecture/BaseApplication.kt
+++ app/src/main/java/com/example/loquicleanarchitecture/BaseApplication.kt
@@ -5,7 +5,6 @@ import dagger.android.DaggerApplication
 
 class BaseApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<DaggerApplication> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
